dd7203527cfc32145a11b0250c322585dfabe4b2
Asif Jirayat <asifjirayat@gmail.com>
2025-08-20 16:55:46 +0530

    Button border added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,15 @@
 const Button = ({ children, onClick, type }) => {
-  let styleClasses = "p-4 rounded cursor-pointer";
+  let styleClasses = "p-4 rounded cursor-pointer border-1";
 
   switch (type) {
     case "increment":
-      styleClasses += " bg-green-100 text-green-700";
+      styleClasses += " bg-green-100 text-green-700 border-green-300";
       break;
     case "decrement":
-      styleClasses += " bg-red-100 text-red-700";
+      styleClasses += " bg-red-100 text-red-700 border-red-300";
       break;
     case "reset":
-      styleClasses += " bg-stone-100 text-stone-700";
+      styleClasses += " bg-stone-100 text-stone-700 border-stone-300";
       break;
   }
 
