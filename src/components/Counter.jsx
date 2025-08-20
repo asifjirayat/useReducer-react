8ca6c183801661973d035038f22b1db568c0845e
Asif Jirayat <asifjirayat@gmail.com>
2025-08-20 17:14:56 +0530

    Migrated from useState to useReducer hook.

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,39 +1,36 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import Button from "./Button.jsx";
+import { counterReducer } from "../store/counterReducer.jsx";
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
-
-  const incrementCount = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
-
-  const decrementCount = () => {
-    setCount((prevCount) => prevCount - 1);
-  };
-
-  const resetCount = () => {
-    setCount(0);
-  };
+  const [countState, countDispatch] = useReducer(counterReducer, {
+    count: 0,
+  });
 
   let countStyles = "text-stone-700";
-  if (count > 0) countStyles = "text-green-700";
-  if (count < 0) countStyles = "text-red-700";
+  if (countState.count > 0) countStyles = "text-green-700";
+  if (countState.count < 0) countStyles = "text-red-700";
 
   return (
     <div className="flex flex-col justify-center items-center">
       <p className="text-4xl">
         The count is:{" "}
-        <span className={`font-bold ${countStyles}`}>{count}</span>
+        <span className={`font-bold ${countStyles}`}>{countState.count}</span>
       </p>
       <div className="flex flex-row gap-4 mt-8">
-        <Button onClick={incrementCount} type="increment">
+        <Button
+          onClick={() => countDispatch({ type: "INCREMENT" })}
+          type="increment"
+        >
           Increment Count
         </Button>
-        <Button onClick={decrementCount} type="decrement">
+        <Button
+          onClick={() => countDispatch({ type: "DECREMENT" })}
+          type="decrement"
+        >
           Decrement Count
         </Button>
-        <Button onClick={resetCount} type="reset">
+        <Button onClick={() => countDispatch({ type: "RESET" })} type="reset">
           Reset Count
         </Button>
       </div>
